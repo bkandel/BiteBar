fbe9edc7d355f5a5402e57c5db099daf2fae27a9
unknown <Ben@Erwin.(none)>
2011-10-26 14:13:01 -0400

    This version retrieves velocities and freqs.
    This version successfully calculates and plots the bandwidths and maximum
    velocities of transients.

--- Retrieve_Vels_Freqs.m
+++ Retrieve_Vels_Freqs.m
@@ -5,7 +5,7 @@ clear; clc; close all;
 %% Define Constants
 Min_Magnitude = 10; 
 % Only analyze transients that generated a 
-% changein position of 5 degs. 
+% changein position of 10 degs. 
 Min_Duration = 20; % ms
 transLengthPadded = 10; 
 % We will pad and window the transients so that 
@@ -66,9 +66,11 @@ for i = 1:length(Data_Calib)
             if (trans_mags{i,j}(k) > Min_Magnitude && trans_lengths{i,j}(k) > Min_Length)
                 transient_pos{i,j}{m(j)} = Position{i}(shift_indices{i,j}(k):shift_indices{i,j}(k+1), j); 
                 % Zero all positions so that they start
-                % from zero position
+                % from zero position. 
+                % Take absolute value--we don't need
+                % negative values. 
                 transient_pos{i,j}{m(j)}(:) =...
-                    transient_pos{i,j}{m(j)}(:) - transient_pos{i,j}{m(j)}(1); 
+                    abs(transient_pos{i,j}{m(j)}(:) - transient_pos{i,j}{m(j)}(1)); 
                 % Retrieve velocity transients, too. 
                 transient_vel{i,j}{m(j)} = Data_Calib{i}(shift_indices{i,j}(k):shift_indices{i,j}(k+1), j);
                 m(j) = m(j) + 1; 
@@ -80,16 +82,45 @@ end
 
 %% Do frequency analysis
 % Pad and cosine window transients
+names{1} = 'Roll'; 
+names{2} = 'Pitch'; 
+names{3} = 'Yaw'; 
+
+figure(1); 
+% This stretch of code is very sloppy--needs to 
+% be better documented, etc.
 for i = 1:length(Data_Calib)
     for j = 1:3
         for k = 1:(m(j) - 1) % m-1 is number of significant transients
             trans_pos_pad{i,j}{k} = CosinePad(transient_pos{i,j}{k}, ...
                 sampling_rate, transLengthPadded, padLength); 
+            [trans_pos_fft{i,j}{k}, freq_indices{i,j}{k}] = positiveFFT(trans_pos_pad{i,j}{k}, sampling_rate);
+            trans_pos_fft{i,j}{k} = abs(trans_pos_fft{i,j}{k}); 
+            trans_pos_pwr{i,j}{k} = cumtrapz(trans_pos_fft{i,j}{k}); 
+            trans_pos_pctpwr{i,j}{k} = trans_pos_pwr{i,j}{k}(:) / trans_pos_pwr{i,j}{k}(end); 
+            index = find(trans_pos_pctpwr{i,j}{k} > 0.99); 
+            trans_pos_99pctpwr{i,j}(k) = freq_indices{i,j}{k}(index(1)); 
         end
+        subplot(i,3,j); 
+        freq_hist{i,j} = histc(trans_pos_99pctpwr{i,j}, 0:1.5:20) / length(trans_pos_99pctpwr{i,j}); 
+        bar(0:1.5:20, freq_hist{i,j}); axis([0 20 0 0.3]); 
+        title(names{j}); xlabel('Frequency'); ylabel('Percent Transients'); 
     end
 end
 
+%% Retrieve max velocities
+figure(2); 
+for i = 1:length(Data_Calib)
+    for j = 1:3
+        for k = 1:(m(j) - 1) % as before
+            max_vels{i,j}(k) = max(abs(transient_vel{i,j}{k})); 
+        end
+        subplot(i,3,j); vel_hist{i,j} = histc(max_vels{i,j}, 0:75:600) / length(max_vels{i,j}); 
+        bar(0:75:600, vel_hist{i,j}); axis([0 650 0 0.5]); 
+        title(names{j}); xlabel('Max Velocity'); 
+        ylabel('Percent Transients'); 
+    end
+end
 
-
-
+        
 
