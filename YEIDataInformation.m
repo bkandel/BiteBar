9aa4cc773e2d81d33ee233c15afc93fec539f2a4
Ben_Kandel <bkandelemail-github@yahoo.com>
2012-12-03 11:14:41 -0500

    Finished processing pipeline; all parts of code are modular.

---
+++ YEIDataInformation.m
@@ -0,0 +1,31 @@
+% This file will describe where all relevant data 
+% collected with the YEI sensor is and what 
+% format it was stored in. 
+
+data(1).Directory = '../../Data/GrossmanTests/GrossmanTests/AllData/'; 
+data(1).Format = 'Text'; 
+data(1).CalibrationFile =  '../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
+
+data(2).Directory =  '../../Data/YEIJump/'; 
+data(2).Format = 'Text'; 
+data(2).CalibrationFile =  '../../Calibrations/Calibration_20120826/Fitted_Values.mat';
+
+data(3).Directory = '../../Data/YEI_20120820/YEI_Data_20120820/AllData/'; 
+data(3).Format = 'Text'; 
+data(3).CalibrationFile ='../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
+
+data(4).Directory =  '../../Data/GrossmanTests/GrossmanTests/AllData/'; 
+data(4).Format = 'Text'; 
+data(4).CalibrationFile = '../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
+ 
+ data(5).Directory = '../../Data/Grossman_20120913_YEI006/'; 
+ data(5).Format = 'Binary'; 
+ data(5).CalibrationFile = '../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
+
+ data(6).Directory =  '../../Data/Terrain_20120913_YEI006/'; 
+ data(6).Format = 'Binary';
+ data(6).CalibrationFile =  '../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
+ 
+ data(7).Directory = '../../Data/Terrain_20120921_007YEI_008YEI/';
+ data(7).Format = 'Binary'; 
+ data(7).CalibrationFile =  '../../Calibrations/Calibration_20120826/Fitted_Values.mat'; 
