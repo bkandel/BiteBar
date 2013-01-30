39579c86d54ce54e9fe8801c898840a09954e419
bkandel <bkandel-github@yahoo.com>
2013-01-30 09:09:04 -0500

    ENH: Changed variable name.

--- PlotCorrelationDelay.m
+++ PlotCorrelationDelay.m
@@ -4,25 +4,26 @@
 % first, run ReadAndProcessYEIData.m or ReadRotateAndProcessYEIData.m so
 % that you have a "FilteredData" struct.
 close; clear Correlation; 
-j = 241 ; FilteredData(j).Filename
+RunNumber = 407 ; FilteredData(RunNumber).Filename
 % make sure it's an interesting run in unconstrained conditions.  
 
 MaxOffset = 125; 
 for Offset = 1:MaxOffset
-tmp = corrcoef(FilteredData(j).XGyro, circshift(FilteredData(j).YGyro', Offset)'); 
+tmp = corrcoef(FilteredData(RunNumber).XGyro, ...
+    circshift(FilteredData(RunNumber).YGyro', Offset)'); 
 Correlation(Offset) = tmp(2, 1); 
 end
 
 DelayIndices = 1:MaxOffset; 
-TimeStepVector = FilteredData(j).TimeInSeconds - ...
-    circshift(FilteredData(j).TimeInSeconds', 1)'; 
+TimeStepVector = FilteredData(RunNumber).TimeInSeconds - ...
+    circshift(FilteredData(RunNumber).TimeInSeconds', 1)'; 
 MeanTimeStep = mean(TimeStepVector(2:end)); 
 DelayInSeconds = DelayIndices .* MeanTimeStep; 
 
 plot(DelayInSeconds, Correlation, 'LineWidth', 2); 
 ylim([-0.2 1])
 title(strcat('Correlation Between Roll and Pitch as Function of Delay', ...
-    FilteredData(j).Filename) ); 
+    FilteredData(RunNumber).Filename) ); 
     %'FontSize', 16)
 xlabel('Delay (s)', 'FontSize', 14)
 ylabel('Correlation', 'FontSize', 14)
